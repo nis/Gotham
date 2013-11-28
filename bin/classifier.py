b4c48a4436b19952d3ef04961a4676a154fc4c5c
nis <nissarup@NisSSD.local>
2013-11-28 14:28:13 +0100

    Classifier now working. Pretty slow, though.

--- bin/classifier.py
+++ bin/classifier.py
@@ -11,6 +11,88 @@ startTime = datetime.datetime.now()
 
 conf = {}
 
+def classify():
+	os.system('clear')
+	# Get or create method
+	try:
+		method = Detection_method.select().where(Detection_method.name == conf['method-name']).get()
+		print 'Fetched Detection_method'
+	except DoesNotExist:
+		method = Detection_method.create(name = conf['method-name'])
+		print 'Created Detection_method'
+
+	# Get or create Event_type
+	try:
+		eventtype = Event_type.select().where(Event_type.name == conf['type']).get()
+		print 'Fetched Event_type'
+	except DoesNotExist:
+		eventtype = Event_type.create(name = conf['type'])
+		print 'Created Event_type'
+
+	# Get linenumbers for positive events
+	with open(conf['lines'], 'r') as f:
+		positive_lines = map(int, f.readlines())
+
+	# Walk the detected events
+	linecount = 1
+	recordings_not_found = 0
+	events_not_found = 0
+	classifications_saved = 0
+	classifications_already_saved = 0
+	with open(conf['detected-events'], 'r') as f:
+		for line in f:
+			if linecount in positive_lines:
+				# print line
+				ident = line.split('/')[-1].split('.events2')
+				offset = ident[1].split('=')
+				ident = ident[0]
+				dur_sample = int(offset[-1])
+				offset = int(offset[1].split(':')[0])
+				# print ident, offset, dur_sample
+
+				# Find recording
+				try:
+					recording = Recording.select().where( 
+						Recording.filename == ident).get()
+				except DoesNotExist:
+					# print 'Recording not in DB. Skipping event.'
+					recordings_not_found = recordings_not_found + 1
+					continue
+
+				try:
+					event = Event.select().where(	Event.recording == recording, 
+													Event.offset == offset, 
+													Event.duration_sample == dur_sample).get()
+				except DoesNotExist:
+					# print 'Event not found in DB. Skipping event.'
+					events_not_found = events_not_found + 1
+					continue
+
+				try:
+					classif = Event_classification.select().where(
+						Event_classification.event == event,
+						Event_classification.detection_method == method,
+						Event_classification.event_type == eventtype).get()
+					print 'Classification already saved.'
+					classifications_already_saved = classifications_already_saved + 1
+				except DoesNotExist:
+					classif = Event_classification.create(
+						event = event,
+						detection_method = method,
+						event_type = eventtype
+						)
+					classifications_saved = classifications_saved + 1
+					print 'Classification saved.'
+
+			
+			linecount = linecount + 1
+
+		print (linecount - 1), 'lines processed.'
+		print recordings_not_found, 'recordings not found.'
+		print events_not_found, 'events not found.'
+		print classifications_already_saved, 'classifications already in the DB.'
+		print classifications_saved, 'new classifications saved.'
+
 def main(argv):
 	try:
 		opts, args = getopt.getopt(argv,"hd:l:m:t:",['detected-events=', 'lines=', 'method-name=', 'type='])
@@ -29,7 +111,7 @@ def main(argv):
 		elif opt in ('-m', '--method-name'):
 			conf['method-name'] = arg
 		elif opt in ('-t', '--type'):
-			conf['type'] = True
+			conf['type'] = arg
 
 	# Check arguments
 	try:
@@ -64,9 +146,10 @@ def main(argv):
 		print 'The file', conf['lines'], 'could not be found.'
 		sys.exit(2)
 
+	classify()
+
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
 
-print(datetime.datetime.now()-startTime)
-print 'Jobs done!'
+print 'Jobs done in', datetime.datetime.now()-startTime
